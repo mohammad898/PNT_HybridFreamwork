c3bd8ff59ddfc3e5288ddeb266d21556b68a2bb7
mnihussain <md.nasiribnehussain@gmail.com>
2018-09-16 23:09:22 -0400

    adding ReusableAPI as CommonAPI

--- Generic/src/main/java/base/ReusableAPI.java
+++ Generic/src/main/java/base/ReusableAPI.java
@@ -1,8 +1,27 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
 
 public class ReusableAPI {
     public static WebDriver driver = null;
+
+    @BeforeMethod
+    public void setDriver (String url){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mdnas\\eclipse-workspace\\GroupBlueFrameWork\\Generic\\DriversForBrowser\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.get(url);
+    }
+    @AfterMethod
+    public void closeTest(){
+        driver.close();
+    }
+
 
 }
