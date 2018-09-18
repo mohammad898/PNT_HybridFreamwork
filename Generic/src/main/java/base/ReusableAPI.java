f757dfc0631eca10cb887608c1dff42af1c89a85
mnihussain <md.nasiribnehussain@gmail.com>
2018-09-17 20:36:31 -0400

    just checking

--- Generic/src/main/java/base/ReusableAPI.java
+++ Generic/src/main/java/base/ReusableAPI.java
@@ -4,12 +4,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import java.util.concurrent.TimeUnit;
 
 public class ReusableAPI {
     public static WebDriver driver = null;
 
+    @Parameters
     @BeforeMethod
     public void setDriver (String url){
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\mdnas\\eclipse-workspace\\GroupBlueFrameWork\\Generic\\DriversForBrowser\\chromedriver.exe");
