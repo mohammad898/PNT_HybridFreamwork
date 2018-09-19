11db26c4ff3b3565fa376d0cc1606c82f0a10788
afia <afiafarjana560@gmail.com>
2018-09-19 12:29:45 -0400

    added a new class

--- Generic/src/main/java/base/ReusableAPI.java
+++ Generic/src/main/java/base/ReusableAPI.java
@@ -1,12 +1,10 @@
 package base;
 
-import com.beust.jcommander.Parameter;
-import com.beust.jcommander.Parameters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.TestNGException;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,13 +13,25 @@ public class ReusableAPI { //Remember this class is not reading from that TestRu
     public static WebDriver driver = null;
 
     @BeforeMethod
-    public void setDriver () {
+    public void setDriver(String os,String url) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\mdnas\\eclipse-workspace\\GroupBlueFrameWork\\Generic\\DriversForBrowser\\chromedriver.exe");
         driver = new ChromeDriver();
+
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+
         driver.get("https://www.airbnb.com/");
     }
+    /*public static void setDriverForOS(String os){
+        if(os.equalsIgnoreCase("Windows")){
+          System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir"+"\\Generic\\DriversForBrowser\\chromedriver.exe"));
+          driver = new ChromeDriver();
+        }
+        else if(os.equalsIgnoreCase("Mac")){
+           System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/Generic/DriversForBrowser/chromedriverMacOS");
+            driver = new ChromeDriver();
+        }
+    }*/
 
     @AfterMethod
     public void closeTest(){
