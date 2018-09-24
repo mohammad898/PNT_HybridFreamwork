be4116edf8de08c941c15bd8742ca4cae3f422a4
mnihussain <md.nasiribnehussain@gmail.com>
2018-09-23 23:51:27 -0400

    trying to shaping

--- Generic/src/main/java/reporting/ApplicationLog.java
+++
@@ -1,9 +0,0 @@
-package reporting;
-
-public class
-ApplicationLog {
-    public static void epicLogger(String epic){
-        TestLogger.log(epic);
-        TestLogger.log("launch local browser");
-    }
-}
